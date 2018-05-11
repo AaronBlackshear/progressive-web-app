ba685eeaf267a1cb6de5be236a7da0fe07270e23
shanellewebster <webstershanelle@gmail.com>
2018-05-11 12:22:25 -0500

    2

---
+++ progressive-web-app/public/server/controllers/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mc = require("./Ctrl");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/api/weather", mc.getWeather);
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`Live on port ${port}`);
+});
