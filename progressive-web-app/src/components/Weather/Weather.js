836db3c7e84d883ae00fc33107b9069b5c4b3030
Aaron Blackshear <theshiftybapple@gmail.com>
2018-05-11 13:41:26 -0500

    Kelvin Math working

--- progressive-web-app/src/components/Weather/Weather.js
+++ progressive-web-app/src/components/Weather/Weather.js
@@ -1,26 +1,30 @@
 import React, { Component } from "react";
 
 export default class Weather extends Component {
-//   componentDidMount() {
-//     this.props.weatherData.data &&
-//       console.log(
-//         this.props.weatherData.data.city.name,
-//         this.props.weatherData.data.list[0].main.temp
-//       );
+//   constructor() {
+//     super();
+//     this.state = {
+//       fahrenheit: ""
+//     };
 //   }
 
   render() {
-    this.props.weatherData.data &&
-    console.log(
-      this.props.weatherData.data.city.name,
-      this.props.weatherData.data.list[0].main.temp
+    // this.props.weatherData.data &&
+    // console.log(
+    //   this.props.weatherData.data.city.name,
+    //   this.props.weatherData.data.list[0].main.temp
+
+    let fahrenheit;
+    this.props.weatherData.data && (
+        (fahrenheit = (9 / 5) * (this.props.weatherData.data.list[0].main.temp - 273) + 32) 
     );
     return (
-        <div>
-            {this.props.weatherData.data && (
-                <div>{this.props.weatherData.data.city.name}</div>
-            )}
-        </div>
+      <div>
+        {this.props.weatherData.data && (
+          <div>{this.props.weatherData.data.city.name}</div>
+        )}
+        {Math.floor(fahrenheit) || null}
+      </div>
     );
   }
 }
